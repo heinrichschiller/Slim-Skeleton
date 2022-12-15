b65df01a7b945f7492cd33affc1b2fe0eb87eaa0
heinrichschiller <heinrich@heinrich-schiller.de>
2022-12-15 12:07:30 +0100

    added, Config and ErrorMiddleware

--- config/container.php
+++ config/container.php
@@ -2,6 +2,7 @@
 
 declare(strict_types=1);
 
+use App\Support\Config;
 use Psr\Container\ContainerInterface;
 use Slim\App;
 use Slim\Factory\AppFactory;
@@ -11,6 +12,13 @@ return [
     'settings' => function () 
     {
         return require __DIR__ . 'settings.php'; 
+    },
+
+    Config::class => function (ContainerInterface $container): Config
+    {
+        $settings = $container->get('settings');
+
+        return new Config($settings);
     },
 
     App::class => function (ContainerInterface $container) 
@@ -24,5 +32,15 @@ return [
     {
         $settings = $container->get('settings')['error'];
         $app = $container->get(App::class);
-    }
+
+        $errorMiddleware = new ErrorMiddleware(
+            $app->getCallableResolver(),
+            $app->getResponseFactory(),
+            (bool) $settings['display_error_details'],
+            (bool) $settings['log_errors'],
+            (bool) $settings['log_error_details']
+        );
+        
+        return $errorMiddleware;
+    },
 ];
