a67f94ef5d4131d13527d83187d0205e7ba6ac6e
heinrichschiller <heinrich@heinrich-schiller.de>
2022-12-15 12:08:36 +0100

    parameter explanation

--- src/Action/Home/HomeAction.php
+++ src/Action/Home/HomeAction.php
@@ -9,6 +9,15 @@ use Psr\Http\Message\ServerRequestInterface as Request;
 
 final class HomeAction
 {
+    /**
+     * The invoker
+     * 
+     * @param Request $request Representation of an incoming, server-side HTTP request.
+     * @param Response $response Representation of an outgoing, server-side response.
+     * @param array<string> $args Get all of the route's parsed arguments.
+     * 
+     * @return Response
+     */
     public function __invoke(Request $request, Response $response, array $args = []): Response
     {
         $response->getBody()->write('Hello World!');
