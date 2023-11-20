135dd413907b297d0e454576f453d612a01995d2
heinrichschiller <heinrich@heinrich-schiller.de>
2023-11-20 19:26:21 +0100

    clean code and advanced settings for errors

--- config/defaults.php
+++ config/defaults.php
@@ -4,6 +4,8 @@ error_reporting(0);
 ini_set('display_errors', '0');
 ini_set('display_startup_errors', '0');
 
+// List of Supported Timezones
+// see, https://www.php.net/manual/en/timezones.php
 date_default_timezone_set('Europe/Berlin');
 
 $settings = [];
@@ -12,10 +14,15 @@ $settings = [];
 $settings['error'] = [
     // Should be set to false for the production environment
     'display_error_details' => false,
+
     // Should be set to false for the test environment
     'log_errors' => true,
+
     // Display error details in error log
     'log_error_details' => true,
+
+    // The error logfile
+    'log_file' => 'error.log',
 ];
 
 $settings['logger'] = [
