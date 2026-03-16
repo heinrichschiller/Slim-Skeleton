4097c6117d2fae2d51a3da61d201108ab0554d7f
heinrichschiller <heinrich@heinrich-schiller.de>
2026-03-16 12:29:40 +0100

    php-version test moved to index.php

--- config/bootstrap.php
+++ config/bootstrap.php
@@ -7,17 +7,6 @@ use Slim\App;
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
-if (PHP_VERSION_ID < 80400) {
-    $message = 'This Slim-Skeleton is supported from PHP 8.4.0 or higher. Installed PHP version is: ' . PHP_VERSION;
-
-    if (PHP_SAPI === 'cli') {
-        fwrite(STDERR, $message . PHP_EOL);
-        exit(1);
-    }
-
-    exit($message);
-}
-
 $container = (new ContainerBuilder())
     ->addDefinitions(__DIR__ . '/container.php')
     ->build();
--- public/index.php
+++ public/index.php
@@ -2,4 +2,15 @@
 
 declare(strict_types=1);
 
+if (PHP_VERSION_ID < 80400) {
+    $message = 'This Slim-Skeleton is supported from PHP 8.4.0 or higher. Installed PHP version is: ' . PHP_VERSION;
+
+    if (PHP_SAPI === 'cli') {
+        fwrite(STDERR, $message . PHP_EOL);
+        exit(1);
+    }
+
+    exit($message);
+}
+
 (require_once __DIR__ . '/../config/bootstrap.php')->run();
